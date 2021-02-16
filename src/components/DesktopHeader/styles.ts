9754e7a59ad31f04ab1bf9d8f621c91c4b9ba6b3
fsluizvictor <fs.luizvictor@gmail.com>
2021-02-16 01:03:12 -0300

    add any components in Linkedins's ui clone

---
+++ src/components/DesktopHeader/styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+import {} from 'react-icons/gr'
+
+export const Container = styled.div`
+  
+`;
+
+export const Wrapper = styled.div`
+`
+export const LinkedinIcon = styled()`
+`
+export const SearchInput = styled()`
+`
+export const HomeIcon = styled()`
+`
+export const NotificationsIcon = styled()`
+`
+export const ProfileCircle = styled()`
+`
+export const CaretDownIcon = styled()`
+`
